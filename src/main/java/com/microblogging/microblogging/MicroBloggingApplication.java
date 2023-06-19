6659da533c5a61750d382c28e3b852cd61f600d2
SaketSaxena <Cr!m!n@l0>
2023-06-19 08:38:10 +0200

    Add initial setup for spring boot application

---
+++ src/main/java/com/microblogging/microblogging/MicroBloggingApplication.java
@@ -0,0 +1,13 @@
+package com.microblogging.microblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBloggingApplication.class, args);
+	}
+
+}
